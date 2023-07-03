5ec82faea61216659ded862897ebebe728fdbec9
Natalia Grela <nataliagrela83@mail.com>
2023-07-03 18:51:24 +0200

    fixed

--- src/slice/phonebook.js
+++ src/slice/phonebook.js
@@ -25,25 +25,6 @@ const slice = createSlice({
     filter: '',
   },
   reducers: {
-    loadContactsLocalStorage: state => {
-      const storedContacts = localStorage.getItem('contacts');
-      const defaultContacts = [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ];
-      if (storedContacts) {
-        let contacts = JSON.parse(storedContacts);
-        if (contacts.length === 0) {
-          contacts = defaultContacts;
-        }
-        state.contacts = contacts;
-      } else {
-        state.contacts = defaultContacts;
-      }
-    },
-
     updateFilter: (state, action) => {
       const { query } = action.payload;
       state.filter = query;
